10c879b14b38571701115fe3ecc46f08a70e2a47
Vanchik1 <vankabananka@gmail.com>
2019-03-31 21:41:30 -0400

    Created main class for Assignment 3

---
+++ src/Arrays/src/SortingCountries.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+//TODO create Javadoc
+
+public class SortingCountries
+{
+    private ArrayList<String> countryList = new ArrayList();
+    private ArrayList<String> capitalList = new ArrayList();
+    private ArrayList<String> populationList = new ArrayList();
+    private ArrayList<String> areaList = new ArrayList();
+
+    //TODO create a sortingCountries constructor
+    //TODO create a file-reading method
+
+
+    public static void main (String[] args)
+    {
+        //TODO create an instance of sortingCountries
+    }
+}
