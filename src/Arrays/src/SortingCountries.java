d884d94a1fe395f5f565202b548be4de64ae3626
Vanchik1 <vankabananka@gmail.com>
2019-04-01 23:28:52 -0400

    Updated SortingCountries.java and added Countries-Populations.txt

--- src/Arrays/src/SortingCountries.java
+++ src/Arrays/src/SortingCountries.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //TODO create Javadoc
 
@@ -9,12 +12,37 @@ public class SortingCountries
     private ArrayList<String> populationList = new ArrayList<String>();
     private ArrayList<String> areaList = new ArrayList<String>();
 
-    //TODO create a sortingCountries constructor
-    //TODO create a file-reading method
+    public SortingCountries()
+    {
+        readFile();
+    }
 
+    //TODO create a file-reading method
+    private void readFile()
+    {
+        try
+        {
+            Scanner inFile = new Scanner(new FileInputStream(new File("Countries-Population.txt")));
+            while (inFile.hasNextLine())
+            {
+                String line = inFile.nextLine();
+                int index = line.indexOf(" ");
+                System.out.println(line.substring(0,index));
+                //TODO put countries, capitals, populations and areas into their respective lists
+            }
+            inFile.close();
+        }
+        catch (FileNotFoundException f)
+        {
+            System.out.println("There's no such File!");
+        }
+    }
+
+    //TODO create a methods that write to sortedByCountry.txt(not yet created) and sortedByPopulations.txt (not yet created)
 
     public static void main (String[] args)
     {
-        //TODO create an instance of sortingCountries
+        SortingCountries sc = new SortingCountries();
+        sc.readFile();
     }
 }
