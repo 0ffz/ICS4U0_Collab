deb2e1926b634fd854ddd59474b2fea3127fb8f0
Vanchik1 <vankabananka@gmail.com>
2019-04-05 00:11:51 -0400

    added comments

--- src/Arrays/src/SortingCountries.java
+++ src/Arrays/src/SortingCountries.java
@@ -7,12 +7,21 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 /**
+ This class creates the 4 Arraylists which will store the attributes of each country.
+ * It reads from the file Countries-Population, distributes the attributes to their respective lists,
+ * sorts the country names and populations and writes both variations of the sort into a txt file it creates.
+ *
+ * <h2>Course Info:<h2/>
+ * ICS4U0, Krasteva, V.
+ *
+ * @version 2
+ * @author Ivan Karlov,  addCountry by Daniel Vozny and MergeSort by Enfei Zhang
  *
  */
 
 public class SortingCountries {
-    String[] multiWordCountries = {"Antigua and Barbuda", "Bosnia and Herzegovina", "Brunei Darussalam", "Burkina Faso", "Cabo Verde", "Cape Verde", "Central African Republic", "Congo, Democratic Republic of the", "Congo, Republic of", "Costa Rica", "Czech Republic", "Côte D'Ivoire", "Côte d'Ivoire", "Dominican Republic", "East Timor", "El Salvador", "Equatorial Guinea", "Guinea Bissau", "Korea, North", "Korea, South", "Marshall Islands", "Myanmar (Burma)", "New Zealand", "Papua New Guinea", "Saint Lucia", "San Marino", "Saudi Arabia", "Sierra Leone", "Solomon Islands", "South Africa", "South Sudan", "Sri Lanka", "St. Kitts and Nevis", "St. Lucia", "St. Vincent and the Grenadines", "São Tomé and Príncipe", "Trinidad and Tobago", "United Arab Emirates", "United Kingdom", "United States", "Vatican City", "Western Sahara"};
-    private ArrayList<String> countryList = new ArrayList<String>();
+    String[] multiWordCountries = {"Antigua and Barbuda", "Bosnia and Herzegovina", "Brunei Darussalam", "Burkina Faso", "Cabo Verde", "Cape Verde", "Central African Republic", "Congo, Democratic Republic of the", "Congo, Republic of", "Costa Rica", "Czech Republic", "Côte D'Ivoire", "Côte d'Ivoire", "Dominican Republic", "East Timor", "El Salvador", "Equatorial Guinea", "Guinea Bissau", "Korea, North", "Korea, South", "Marshall Islands", "Myanmar (Burma)", "New Zealand", "Papua New Guinea", "Saint Lucia", "San Marino", "Saudi Arabia", "Sierra Leone", "Solomon Islands", "South Africa", "South Sudan", "Sri Lanka", "St. Kitts and Nevis", "St. Lucia", "St. Vincent and the Grenadines", "São Tomé and Príncipe", "Trinidad and Tobago", "United Arab Emirates", "United Kingdom", "United States", "Vatican City", "Western Sahara"};//array containing country names made up of multiple words
+    private ArrayList<String> countryList = new ArrayList<String>(); //ArrayLists containing the country's names, capitals, populations and areas, respectively
     private ArrayList<String> capitalList = new ArrayList<String>();
     private ArrayList<String> populationList = new ArrayList<String>();
     private ArrayList<String> areaList = new ArrayList<String>();
@@ -27,7 +36,7 @@ public class SortingCountries {
         try {
             Scanner inFile = new Scanner(new FileInputStream(new File("Countries-Population.txt")));
             while (inFile.hasNextLine()) {
-                String line = inFile.nextLine().replace(",", "");
+                String line = inFile.nextLine().replace(",", "");//creates string to contain the line and removes commas from area and population
 
                 addCountry(line);
             }
@@ -38,15 +47,15 @@ public class SortingCountries {
     }
 
     public void sortAndWrite() {
-        ArrayList<Integer> tempPosList = new ArrayList<>();
-        ArrayList<Integer> tempPopList = new ArrayList<>();
-        Comparator<String> comp = new Comparator<String>() {
+        ArrayList<Integer> tempPosList = new ArrayList<>(); //ArrayList containing the positions of countryList elements within countryList
+        ArrayList<Integer> tempPopList = new ArrayList<>(); //ArrayList containing Integer values of each country's population
+        Comparator<String> comp = new Comparator<String>() { //creates Comparator object to be used in the sort method
             @Override
             public int compare(String a, String b) {
                 return a.compareTo(b);
             }
         };
-        Comparator<Integer> comp2 = new Comparator<Integer>() {
+        Comparator<Integer> comp2 = new Comparator<Integer>() { //creates Comparator object for second sort
             @Override
             public int compare(Integer integer, Integer t1) {
                 return integer.compareTo(t1);
@@ -57,7 +66,7 @@ public class SortingCountries {
         try {
             PrintWriter writer = new PrintWriter(new FileWriter("sortedByCountry.txt"));
             for (int i = 0; i < countryList.size(); i++) {
-                writer.println(countryList.get(i) + "\t\t\t" + populationList.get(i));
+                writer.println(countryList.get(i) + "\t\t\t" + populationList.get(i)); //writes sorted data into "sortedByCountry.txt"
             }
             writer.close();
         } catch (IOException e) {
@@ -74,7 +83,7 @@ public class SortingCountries {
         for (int i = 0; i < tempPopList.size(); i++) {
             String population = String.valueOf(tempPopList.get(i));
             String temp = countryList.get(i);
-            populationList.set(i, population);
+            populationList.set(i, population); //adds populations to populationList in sorted order
             countryList.set(i, countryList.get(tempPosList.get(i)));
             countryList.set(tempPosList.get(i), temp);
 
@@ -86,7 +95,7 @@ public class SortingCountries {
         try {
             PrintWriter writer = new PrintWriter(new FileWriter("sortedByPopulation.txt"));
             for (int i = 0; i < countryList.size(); i++) {
-                writer.println(countryList.get(i) + "\t\t\t" + populationList.get(i));
+                writer.println(countryList.get(i) + "\t\t\t" + populationList.get(i)); //writes sorted data into "sortedByPopulation.txt"
             }
             writer.close();
         } catch (IOException e) {
