40c807526c13628c66245c296c514e65ec8cd554
Vanchik1 <vankabananka@gmail.com>
2019-04-05 15:03:11 -0400

    Started combSort

---
+++ src/2. Sorting Presentation/CombSort.java
@@ -0,0 +1,20 @@
+public class CombSort
+{
+    int[] unsorted;
+
+    public void sort (unsorted)
+    {
+        int gap = unsorted.length;
+        double shrinkFactor = 1.3;
+//TODO make actual algorithm lul
+        for (int i = 0; i < unsorted.length; i++)//change to while loop
+        {
+
+        }
+    }
+
+    public static void main (String[] args)
+    {
+        CombSort cs = new ComboSort;
+    }
+}
